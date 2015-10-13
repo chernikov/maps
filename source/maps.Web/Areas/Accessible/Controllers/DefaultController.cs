8ef3ae875a0aa255a6a1821447f7213e04cbf252
Andriy Chernikov <chernikov@gmail.com>
2015-10-13 17:06:20 +0300

    Accessible #2

--- source/maps.Web/Areas/Accessible/Controllers/DefaultController.cs
+++ source/maps.Web/Areas/Accessible/Controllers/DefaultController.cs
@@ -1,13 +1,23 @@
 ﻿using maps.Web.Controllers;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace maps.Web.Areas.Accessible.Controllers
 {
     public class DefaultController : BaseController
     {
+        protected override void Initialize(RequestContext requestContext)
+        {
+            CultureInfo ci = new CultureInfo("uk");
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+            Thread.CurrentThread.CurrentCulture = ci;
+            base.Initialize(requestContext);
+        }
     }
 }
